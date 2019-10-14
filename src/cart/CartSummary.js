ce7df92cea0d124ff6ac6a8ae0b6d2730dc4ec8b
Daniel Bright <brahyt@gmail.com>
2019-10-14 16:45:53 -0700

    refactor done

---
+++ src/cart/CartSummary.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+
+
+function CartSummary (props) {
+
+	const summary = Object.keys(props.selected).map((feature, idx) => {
+		const featureHash = feature + '-' + idx;
+		const selectedOption = props.selected[feature];
+
+		return (
+			<div className="summary__option" key={featureHash}>
+				<div className="summary__option__label">{feature} </div>
+				<div className="summary__option__value">{selectedOption.name}</div>
+				<div className="summary__option__cost">
+					{props.inDollar.format(selectedOption.cost)}
+				</div>
+			</div>
+		);
+	});
+
+  return <div>{summary}</div>
+}
+
+export default CartSummary
