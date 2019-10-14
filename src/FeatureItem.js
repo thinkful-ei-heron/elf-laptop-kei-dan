498e62e83c2eb9259bcfd0054f265b9d320383bd
Daniel Bright <brahyt@gmail.com>
2019-10-14 15:15:45 -0700

    changes to code

--- src/FeatureItem.js
+++ src/FeatureItem.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import slugify from 'slugify';
 
-function FeatureItem (props){
-  return ( 
-    <div key={props.itemHash}>
-            <input
-              type="radio"
-              id={props.itemHash}
-              className="feature__option"
-            />
-          <div key={props.itemHash} className="feature__item">
-            <label htmlFor={props.itemHash} className="feature__label">
-            </label>
-          </div>
-    </div>
-  )
+function FeatureItem (props){ 
+    const options = props.feature.map(item => {
+  console.log(props.selected, item.name)
+      return (
+        <div key={props.key} className="feature__item">
+          <input
+            type="radio"
+            id={props.key}
+            className="feature__option"
+            /*name={slugify(props.feature)}*/
+            checked={item.name === props.selected}
+            onChange={e => props.updateFeature(props.feature, item)}
+          />
+          <label htmlFor={props.key} className="feature__label">
+            {item.name}
+          </label>
+        </div>
+      )
+   })
+  return <div>{options}</div>
 }
-
 export default FeatureItem;
--- src/Features.js
+++ src/Features.js
@@ -2,34 +2,51 @@ import React from 'react';
 import FeatureItem from './FeatureItem';
 import slugify from 'slugify';
 
+//function Features (props) {
+//    const features = Object.keys(props.features).map((feature, idx) => {
+//      const featureHash = feature + '-' + idx;
+//      const options = props.features[feature].map(item => {
+//        const itemHash = slugify(JSON.stringify(item));
+//        return (
+//          <div key={itemHash} className="feature__item">
+//            <input
+//              type="radio"
+//              id={itemHash}
+//              className="feature__option"
+//              name={slugify(feature)}
+//              checked={item.name === props.selected[feature].name}
+//              onChange={e => props.updateFeature(feature, item)}
+//            />
+//            <label htmlFor={itemHash} className="feature__label">
+//              {item.name}
+//            </label>
+//          </div>
+//        );
+//      });
+//
+//      return (
+//        <fieldset className="feature" key={featureHash}>
+//          <legend className="feature__name">
+//            <h3>{feature}</h3>
+//          </legend>
+//          {options}
+//        </fieldset>
+//      );
+//    });
+
 function Features (props) {
     const features = Object.keys(props.features).map((feature, idx) => {
       const featureHash = feature + '-' + idx;
-      const options = props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === props.selected[feature].name}
-              onChange={e => props.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name}
-            </label>
-          </div>
-        );
-      });
-
       return (
         <fieldset className="feature" key={featureHash}>
           <legend className="feature__name">
             <h3>{feature}</h3>
           </legend>
-          {options}
+          <FeatureItem feature={props.features[feature]}
+                       key={featureHash}
+                       selected={props.selected[feature]}
+                       idx={idx}
+          />
         </fieldset>
       );
     });
